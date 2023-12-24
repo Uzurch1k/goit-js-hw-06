53151dd2047fa30ee3a4813ff22351499961a965
Uzurch1k <galcenkovlad7@gmail.com>
2023-12-24 22:17:30 +0200

    fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,28 @@
 //====================================================================
-const getUsersWithFriend = (users, friendName) =>
-  users.filter(user => user.friends.includes(friendName));
+class Storage {
+  #items;
+
+  constructor(arrItems) {
+    this.#items = arrItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
 //====================================================================
 
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
